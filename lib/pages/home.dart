6c11ef8c382cdf35f558b4eb3be4f7dbac793998
abrego <ale.abrego1214@gmail.com>
2022-11-14 23:16:39 -0600

    Finalizando cambios en pantalla

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:adivinaapp/pages/flipcardjuego.dart';
 import 'package:adivinaapp/widgets/boxcontainer.dart';
 import 'package:flutter/material.dart';
 
@@ -46,3 +47,40 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+
+class Detalle {
+  String nombre;
+  Color primarycolor;
+  Color secomdarycolor;
+  Widget goto;
+  int noOfstar;
+
+  Detalle(
+      {
+      required this.nombre,
+      required this.primarycolor,
+      required this.secomdarycolor,
+      required this.noOfstar,
+      required this.goto});
+}
+
+List<Detalle> _lista = [
+  Detalle(
+      nombre: "Fácil",
+      primarycolor: Colors.green,
+      secomdarycolor: Color.fromARGB(255, 129, 199, 132),
+      noOfstar: 1,
+      goto: flipcardgame(Nivel.Facil)),
+  Detalle(
+      nombre: "Medio",
+      primarycolor: Colors.orange,
+      secomdarycolor: Color.fromARGB(255, 255, 183, 77),
+      noOfstar: 2,
+      goto: flipcardgame(Nivel.Medio)),
+  Detalle(
+      nombre: "Difícil",
+      primarycolor: Colors.red,
+      secomdarycolor: Color.fromARGB(255, 229, 115, 115),
+      noOfstar: 3,
+      goto: flipcardgame(Nivel.Dificil)),
+];
